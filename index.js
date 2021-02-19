f9ee322566f9b0acfa5dadcdb06ef7908a30f16a
pulkit@neu <pulkit12083@iiitd.ac.in>
2021-02-19 06:17:27 +0530

    changing cover photo

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var counter = 0;
 
 var img = [
+    "img_made/perchedonatree.jpeg",
     "img_made/20210126_131522.jpg",
     "img_made/20210126_131525.jpg",
     "img_made/IMG_20210123_180946.jpg",
     "img_made/hand.jpeg",
     "img_made/photo_2020-10-04_20-41-04.jpg",
-    "img_made/perchedonatree.jpeg",
     "img_made/photo_2020-05-05_03.15.59.jpeg",
     "img_made/imongrass.jpeg",
     "img_made/photo_2020-10-04_22-16-16.jpg",
