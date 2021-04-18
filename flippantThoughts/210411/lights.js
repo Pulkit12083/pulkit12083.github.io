ee2fa3133b0be011a3e870b00c0423c1209fa109
pulkit@neu <pulkit12083@iiitd.ac.in>
2021-04-18 22:04:03 +0530

    testing dynamic script

--- flippantThoughts/210411/lights.js
+++ flippantThoughts/210411/lights.js
@@ -3,7 +3,7 @@ setbackground();
 
 function setbackground(){
   window.setTimeout( "setbackground()", 1000);
-  newImage = rotate ? 'url(img_made/vlcsnap-2021-04-18-18h59m17s658.png)' : 'url(img_made/vlcsnap-2021-04-18-18h59m20s393.png)';
+  newImage = rotate ? 'url(img_made/vlcsnap-2021-04-18-18h59m17s658.jpg)' : 'url(img_made/vlcsnap-2021-04-18-18h59m20s393.jpg)';
     rotate = !rotate;
     document.body.style.backgroundImage = newImage;
 }
