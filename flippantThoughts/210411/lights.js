0c6912746d556a2bffddd569099a48d9348b0e8e
pulkit@neu <pulkit12083@iiitd.ac.in>
2021-04-18 22:26:25 +0530

    timer length+

--- flippantThoughts/210411/lights.js
+++ flippantThoughts/210411/lights.js
@@ -2,7 +2,7 @@ var rotate = false;
 setbackground();
 
 function setbackground(){
-  window.setTimeout( "setbackground()", 1000);
+  window.setTimeout( "setbackground()", 1500);
   newImage = rotate ? 'url(img_made/vlcsnap-2021-04-18-18h59m17s658.jpg)' : 'url(img_made/vlcsnap-2021-04-18-18h59m20s393.jpg)';
     rotate = !rotate;
     document.body.style.backgroundImage = newImage;
