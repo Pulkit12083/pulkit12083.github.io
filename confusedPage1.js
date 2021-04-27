50c51130f25cf58a357e8b431a7ca3dee49a5735
pulkit@neu <pulkit12083@iiitd.ac.in>
2021-04-27 09:40:42 +0530

    freeing javascript on website

--- confusedPage1.js
+++
@@ -1,9 +0,0 @@
-var $el1;
-var $el2;
-
-setInterval(function() {
-    $el1 = $('.bg-container.active');
-    $el2 = $('.bg-container:not(.active');
-    $el1.removeClass('active');
-    $el2.addClass('active');
-}, 2000);
